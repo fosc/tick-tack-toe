977b52dda3ad246b8acaaa35f8ed0a2bdf19a61d
fosc <foscento@gmail.com>
2021-01-19 10:46:37 -0500

    linting and small refactor

--- opponents.py
+++ opponents.py
@@ -1,20 +1,35 @@
 """This module contains implementations of the opponent interface (play() --> tuple)"""
-class OpponentFactory:
 
+
+class OpponentFactory:
+    """Factory of opponents"""
     def __init__(self):
         self._creators = {}
 
     def register_opponent(self, level, creator):
+        """Register a class with method play() and assign it a difficulty 'level'"""
         self._creators[level] = creator
 
     def get_opponent(self, level):
+        """Get an opponent instance that plays with difficulty 'level'"""
         creator = self._creators.get(level)
         if not creator:
             raise ValueError(level)
         return creator()
 
 
+def max_depth(my_func):
+    """Track recursion depth for a class method based on the current_depth member"""
+    def wrapper(self, *args, **kwargs):
+        self.current_depth += 1
+        res = my_func(self, *args, **kwargs)
+        self.current_depth -= 1
+        return res
+    return wrapper
+
+
 class RecursiveSearchAlgorithm:
+    """Recursively traverses a tree of possible game outcomes and return next move."""
     def __init__(self, max_search_depth):
         self.current_depth = 0
         self.max_search_depth = max_search_depth
@@ -22,17 +37,9 @@ class RecursiveSearchAlgorithm:
     def search_depth_exceeded(self):
         return self.current_depth >= self.max_search_depth
 
-    def max_depth(my_func):
-        def wrapper(self, *args, **kwargs):
-            self.current_depth += 1
-            res = my_func(self, *args, **kwargs)
-            self.current_depth -= 1
-            return res
-
-        return wrapper
-
     @max_depth
     def is_good_move(self, game):
+        """Draws and Wins are both considered equally good by this method"""
         if game.is_game_over()[0]:
             return True  # you cannot loose on your turn - only win or draw
         if self.search_depth_exceeded():
@@ -67,11 +74,16 @@ class RecursiveSearchAlgorithm:
 
 class HumanPlayer:
     @staticmethod
-    def play(game):
+    def get_coordinate(message):
+        try:
+            return int(input(message))
+        except ValueError:
+            print('Invalid input')
+            return HumanPlayer.get_coordinate(message)
+
+    def play(self, game):
         print(game)
-        x = int(input("enter x:"))
-        y = int(input("enter y:"))
-        return x, y
+        return self.get_coordinate("enter x:"), self.get_coordinate("enter y:")
 
 
 class MediumOpponent(RecursiveSearchAlgorithm):
@@ -84,7 +96,7 @@ class HardOpponent(RecursiveSearchAlgorithm):
         super().__init__(5)
 
 
-opponentFactory = OpponentFactory()
-opponentFactory.register_opponent('medium', MediumOpponent)
-opponentFactory.register_opponent('human', HumanPlayer)
-opponentFactory.register_opponent('hard', HardOpponent)
+opponent_factory = OpponentFactory()
+opponent_factory.register_opponent('medium', MediumOpponent)
+opponent_factory.register_opponent('human', HumanPlayer)
+opponent_factory.register_opponent('hard', HardOpponent)
